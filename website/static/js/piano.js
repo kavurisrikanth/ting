80d820e241cdd31152edb5e0def4ea699a46fd3b
Srikanth Kavuri <ksrs9312@gmail.com>
2019-12-22 14:29:50 +0530

    Piano layout done

--- website/static/js/piano.js
+++ website/static/js/piano.js
@@ -8,22 +8,41 @@ function go() {
     console.log("Hello, world");
 }
 
+function drawWhiteKey(first) {
+    let white = document.createElement('li');
+    white.setAttribute('class', 'white' + (first ? '' : ' non_first_key'));
+    return white;
+}
+
+function drawBlackKey() {
+    let black = document.createElement('li');
+    black.setAttribute('class', 'black');
+    return black;
+}
+
+function drawPianoPattern(keysList) {
+    keysList.appendChild(drawWhiteKey(true));
+    keysList.appendChild(drawBlackKey());
+    keysList.appendChild(drawWhiteKey());
+    keysList.appendChild(drawBlackKey());
+    keysList.appendChild(drawWhiteKey());
+
+    keysList.appendChild(drawWhiteKey(true));
+    keysList.appendChild(drawBlackKey());
+    keysList.appendChild(drawWhiteKey());
+    keysList.appendChild(drawBlackKey());
+    keysList.appendChild(drawWhiteKey());
+    keysList.appendChild(drawBlackKey());
+    keysList.appendChild(drawWhiteKey());
+}
+
 function drawPianoSection() {
     let pianoDiv = document.querySelector('.piano-div');
-    pianoDiv.innerHTML = `
-        <ul class="set">
-            <li class="white b"></li>
-            <li class="black as"></li>
-            <li class="white a"></li>
-            <li class="black gs"></li>
-            <li class="white g"></li>
-            <li class="black fs"></li>
-            <li class="white f"></li>
-            <li class="white e"></li>
-            <li class="black ds"></li>
-            <li class="white d"></li>
-            <li class="black cs"></li>
-            <li class="white c"></li>
-        </ul>
-    `;
+    let keysList = document.createElement('ul');
+
+    drawPianoPattern(keysList);
+    drawPianoPattern(keysList);
+    drawPianoPattern(keysList);
+
+    pianoDiv.appendChild(keysList);
 }
